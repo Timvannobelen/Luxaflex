17ccc56d4044192304cf73cba602d2f6555a2312
Haris <6013429@mborijnland.nl>
2021-05-20 11:20:34 +0200

    Breuken pagina toegevoegd

---
+++ website/rekenen1.php
@@ -0,0 +1,121 @@
+<?php
+session_start();
+require 'db_connection.php';
+// CHECK USER IF LOGGED IN
+if(isset($_SESSION['user_email']) && !empty($_SESSION['user_email'])){
+
+$user_email = $_SESSION['user_email'];
+$get_user_data = mysqli_query($db_connection, "SELECT * FROM `users` WHERE user_email = '$user_email'");
+$userData =  mysqli_fetch_assoc($get_user_data);
+
+}else{
+    header('Location: denied.php');
+exit;
+}
+?>
+<!DOCTYPE html>
+<html lang="nl">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <title>Mathmate</title>
+</head>
+<body>
+
+    <nav class="navbar">
+        <div class="container">
+      
+          <div class="navbar-header">
+            <button class="navbar-toggler" data-toggle="open-navbar1">
+              <span></span>
+              <span></span>
+              <span></span>
+            </button>
+            <a href="#">
+              <img src="img/mathmate-logo.png" alt="logo" class="logo">
+            </a>
+          </div>
+      
+          <div class="navbar-menu" id="open-navbar1">
+            <ul class="navbar-nav">
+              <li><a href="index.php">Home</a></li>
+
+              <li><a href="dice.php">Dice</a></li>
+              <li><a href="#">Game</a></li>
+
+              <li class="navbar-dropdown">
+                <a href="#" class="dropdown-toggler" data-dropdown="blog">
+                  Math <i class="fa fa-angle-down"></i>
+                </a>
+                <ul class="dropdown" id="blog">
+                  <li class="active"><a href="#">Breuken</a></li>
+                  <li class="separator"></li>
+                  <li><a href="#">Link 2</a></li>
+                  <li class="separator"></li>
+                  <li><a href="#">Link 3</a></li>
+                  <li class="separator"></li>
+                  <li><a href="#">Link 4</a></li>
+                </ul>
+              </li>
+              <li><a href="signup.php" class="in-icon"><i class="fa fa-user" aria-hidden="true"></i></a></li>
+              <li><a href="logout.php" class="in-icon"><i class="fa fa-sign-out" aria-hidden="true"></i></a></li>
+            </ul>
+          </div>
+        </div>
+      </nav>
+
+      <script>
+        function decfrac(DtF) {
+
+dec=DtF;
+if((isNaN(dec))||(dec=='0.')){
+alert('Het is leeg!');
+df.reset();
+return;
+}
+temp=dec;
+dec=dec.toString().split('.')[1];
+frac=Math.pow(10,dec.length)/parseFloat(dec);
+obj.firstChild.nodeValue='Decimaal getal = '+temp+', is gelijk aan =1/'+frac;
+}
+
+if(window.addEventListener){
+window.addEventListener('load',init,false);
+}
+else { 
+if(window.attachEvent){
+window.attachEvent('onload',init);
+}
+}
+
+function init(){
+df=document.forms[0];
+obj=document.getElementById('fraction');
+df[1].onclick=function() {
+decfrac(document.forms[0][0].value);
+}
+df[2].onclick=function() {
+obj.firstChild.nodeValue='';
+}
+}
+      </script>
+
+      <form action="#">
+      <div class="breuken-input">
+        <label>Decimaal Getal : <input type="text" value='0.'></label>
+        <input class="btn-breuken" type="button" value="Bereken Breuk">
+        <input class="btn-breuken" type="reset" value="Verwijderen">
+      </div>
+      </form>
+
+      <div id="fraction" class="breuken-output">&nbsp;</div>
+
+      <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+      <script src="index.js"></script>
+      <script src="dice.js" ></script>
+
+</body>
+</html>
