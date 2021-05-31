bc0b5408895a251b513b94d038635858d1a7bdd4
Timmety1 <74976881+Timmety1@users.noreply.github.com>
2021-05-31 12:35:02 +0200

    alle mappen goed gezet

---
+++ website/include/navbar.php
@@ -0,0 +1,41 @@
+<nav class="navbar">
+        <div class="container">
+      
+          <div class="navbar-header">
+            <button class="navbar-toggler" data-toggle="open-navbar1">
+              <span></span>
+              <span></span>
+              <span></span>
+            </button>
+            <a href="#">
+              <img src="img/mathmate-logo.png" alt="logo" class="logo">
+            </a>
+          </div>
+      
+          <div class="navbar-menu" id="open-navbar1">
+            <ul class="navbar-nav">
+              <li><a href="index.php">Home</a></li>
+
+              <li class="active"><a href="dice.php">Dice</a></li>
+              <li><a href="#">Game</a></li>
+
+              <li class="navbar-dropdown">
+                <a href="#" class="dropdown-toggler" data-dropdown="blog">
+                  Math <i class="fa fa-angle-down"></i>
+                </a>
+                <ul class="dropdown" id="blog">
+                  <li><a href="rekenen1.php">Talstelsels</a></li>
+                  <li class="separator"></li>
+                  <li><a href="#">Link 2</a></li>
+                  <li class="separator"></li>
+                  <li><a href="#">Link 3</a></li>
+                  <li class="separator"></li>
+                  <li><a href="#">Link 4</a></li>
+                </ul>
+              </li>
+              <li><a href="signup.php" class="in-icon"><i class="fa fa-user" aria-hidden="true"></i></a></li>
+              <li><a href="logout.php" class="in-icon"><i class="fa fa-sign-out" aria-hidden="true"></i></a></li>
+            </ul>
+          </div>
+        </div>
+      </nav>
